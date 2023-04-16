31c9ed36ed48a5383f561267d52bc0faafa1246f
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-04-16 22:02:20 +0300

    adsadas

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTA5NTExODg0NzQyNzczOTczOQ.Gtkq2e.Cmppx8qiDzbpkRCWUwoyW-Fc4bT1tAjjl-BODY',
+    'token': 'MTA5NTExODg0NzQyNzczOTczOQ.G-ESvN.EBol_3ABqvz4YNhKnPRwtt6z9VmLlojwm4UHb0',
     'bot': 'blackHat',
     'id': 1095118847427739739,
     'prefix': '>'
